9edba80b98a278b4c7db8b6bf58dd20e3a54fc02
Christian Gunawan <cg404cgunawan@gmail.com>
2024-10-22 11:03:19 +0700

    Initial Commit

---
+++ VetHereiOS/VetHereiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  VetHereiOSApp.swift
+//  VetHereiOS
+//
+//  Created by Christian Gunawan on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct VetHereiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
